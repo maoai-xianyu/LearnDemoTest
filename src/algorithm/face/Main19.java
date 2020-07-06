5eb596bd9527e88424e5721b71449008b3eaa0ad
zhangkun@boxfish.cn <194264514@qq.com>
2020-07-06 08:15:14 +0800

    测试

---
+++ src/algorithm/face/Main19.java
@@ -0,0 +1,11 @@
+package algorithm.face;
+
+/**
+ * @author zhangkun
+ * @time 2020/7/6 8:14 上午
+ */
+public class Main19 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
